632d9b59987fddd2a8ea08b5f888166dc3fedb9e
ptrickd <patrickdion8@gmail.com>
2024-02-14 18:41:20 -0700

    ProjectsDetails page convert to MUI v5

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -9,95 +9,35 @@ import ImageModal from "./ImageModal";
 import projectsData from "../files/projectsData";
 
 //Constants
-// import { DRAWER_WIDTH } from "../constants/Styling";
+import { DRAWER_WIDTH } from "../constants/Styling";
 
 //Material - UI
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-import {
-  Box,
-  Button,
-  Container,
-  Divider,
-  Grid,
-  // GridList,
-  // GridListTile,
-  Link,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Link from "@mui/material/Link";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
 
-import {
-  useTheme,
-  // styled,
-} from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
-// const drawerWidth = DRAWER_WIDTH;
-// const
-// const useStyles = makeStyles((theme: Theme) =>
-// createStyles({
-// main: {
-//   [theme.breakpoints.up("sm")]: {
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     marginLeft: drawerWidth,
-//   },
-//   padding: theme.spacing(3),
-// },
-// title: {
-//   marginTop: 20,
-// },
-// description: {
-//   minHeight: 50,
-//   marginTop: 20,
-// },
-// links: {
-//   width: "100%",
-//   display: "flex",
-// },
-// button: {
-//   flexGrow: 0,
-// },
-// listItem: {
-//   justifyContent: "center",
-// },
-//
-// imageContainer: {
-//   maxWidth: "100%",
-//   width: "100%",
-//   height: "100%",
-//   // background: '#d0d5f2',
-//   // border: '1px solid black',
-//   // cursor: 'pointer',
-//   overflow: "hidden",
-// },
-// gridList: {
-//   // width: '80%',
-//   // height: '100%',
-// },
-//   })
-// );
-
-// const imgStyles = {
-//   maxWidth: "100%",
-//   maxHeight: "100%",
-//   display: "block",
-//   marginLeft: "auto",
-//   marginRight: "auto",
-//   cursor: "pointer",
-//   marginTop: 10,
-//   marginBottom: 10,
-//   border: "3px solid #d0d5f2",
-// };
+const drawerWidth = DRAWER_WIDTH;
 
 function ProjectDetails() {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState(false);
-  const [image] = useState("");
+  const [image, setImage] = useState("");
   const { id } = useParams();
   const theme = useTheme();
   const widthIsSmall = useMediaQuery(theme.breakpoints.up("sm"));
@@ -106,8 +46,8 @@ function ProjectDetails() {
 
   const {
     title,
-    // screenshots,
-    // screenshotsNum,
+    screenshots,
+    screenshotsNum,
     description,
     skills,
     repo,
@@ -122,19 +62,19 @@ function ProjectDetails() {
     console.log("widthIsLarge", widthIsLarge);
   }, [widthIsSmall, widthIsMedium, widthIsLarge]);
 
-  // const getGridImageCols = () => {
-  //   if (screenshotsNum > 1) {
-  //     if (widthIsLarge) {
-  //       return 3;
-  //     } else if (widthIsMedium) {
-  //       return 2;
-  //     } else if (widthIsSmall) {
-  //       return 1;
-  //     }
-  //     return 1;
-  //   }
-  //   return 1;
-  // };
+  const getGridImageCols = () => {
+    if (screenshotsNum > 1) {
+      if (widthIsLarge) {
+        return 3;
+      } else if (widthIsMedium) {
+        return 2;
+      } else if (widthIsSmall) {
+        return 1;
+      }
+      return 1;
+    }
+    return 1;
+  };
 
   const displayList = () => {
     return skills.map((skill, index) => {
@@ -151,16 +91,27 @@ function ProjectDetails() {
     });
   };
 
-  // const handleOpenModal = (index: number) => {
-  //   setImage(screenshots[index]);
-  //   setOpenModal(true);
-  // };
+  const handleOpenModal = (index: number) => {
+    setImage(screenshots[index]);
+    setOpenModal(true);
+  };
   const handleCloseModal = () => {
     setOpenModal(false);
   };
   return (
-    <Container>
-      <Typography align="center" variant="h4" gutterBottom>
+    <Container
+      sx={{
+        width: { xs: "100%", md: `calc(100% - ${drawerWidth}px)` },
+        marginLeft: { xs: 0, md: drawerWidth },
+        padding: (theme) => theme.spacing(3),
+      }}
+    >
+      <Typography
+        align="center"
+        variant="h4"
+        gutterBottom
+        sx={{ marginTop: 20 }}
+      >
         {title}
       </Typography>
       <span
@@ -170,39 +121,114 @@ function ProjectDetails() {
           alignItems: "center",
         }}
       >
-        {/* <GridList
-          className={classes.gridList}
-          spacing={1}
+        <ImageList
+          sx={{ width: "80%", height: "100%" }}
+          gap={1}
           cols={getGridImageCols()}
         >
-          <GridListTile className={classes.imageContainer}>
+          <ImageListItem
+            sx={{
+              maxWidth: "100%",
+              width: "100%",
+              height: "100%",
+              background: "#d0d5f2",
+              border: "1px solid black",
+              cursor: "pointer",
+              overflow: "hidden",
+            }}
+          >
             <span onClick={() => handleOpenModal(0)} style={{ padding: 5 }}>
-              <img style={imgStyles} src={screenshots[0]} alt="screenshot" />
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: "100%",
+                  maxHeight: "100%",
+                  display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  cursor: "pointer",
+                  marginTop: 10,
+                  marginBottom: 10,
+                  border: "3px solid #d0d5f2",
+                }}
+                src={screenshots[0]}
+                alt="screenshot"
+              />
             </span>
-          </GridListTile>
+          </ImageListItem>
 
           {screenshots[1] && (
-            <GridListTile className={classes.imageContainer}>
+            <ImageListItem
+              sx={{
+                maxWidth: "100%",
+                width: "100%",
+                height: "100%",
+                background: "#d0d5f2",
+                border: "1px solid black",
+                cursor: "pointer",
+                overflow: "hidden",
+              }}
+            >
               <span onClick={() => handleOpenModal(1)} style={{ padding: 5 }}>
-                <img style={imgStyles} src={screenshots[1]} alt="screenshot" />
+                <Box
+                  component="img"
+                  sx={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    display: "block",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    cursor: "pointer",
+                    marginTop: 10,
+                    marginBottom: 10,
+                    border: "3px solid #d0d5f2",
+                  }}
+                  src={screenshots[1]}
+                  alt="screenshot"
+                />
               </span>
-            </GridListTile>
+            </ImageListItem>
           )}
 
           {screenshots[1] && (
-            <GridListTile className={classes.imageContainer}>
+            <ImageListItem
+              sx={{
+                maxWidth: "100%",
+                width: "100%",
+                height: "100%",
+                background: "#d0d5f2",
+                border: "1px solid black",
+                cursor: "pointer",
+                overflow: "hidden",
+              }}
+            >
               <span onClick={() => handleOpenModal(2)} style={{ padding: 5 }}>
-                <img style={imgStyles} src={screenshots[2]} alt="screenshot" />
+                <Box
+                  component="img"
+                  sx={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    display: "block",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    cursor: "pointer",
+                    marginTop: 10,
+                    marginBottom: 10,
+                    border: "3px solid #d0d5f2",
+                  }}
+                  src={screenshots[2]}
+                  alt="screenshot"
+                />
               </span>
-            </GridListTile>
+            </ImageListItem>
           )}
-        </GridList> */}
+        </ImageList>
         <Divider />
         <Typography
           variant="body1"
           paragraph
           align="justify"
-          // className={classes.description}
+          sx={{ minHeight: 50, marginTop: 20 }}
         >
           {description}
         </Typography>
@@ -216,20 +242,17 @@ function ProjectDetails() {
           </List>
         </div>
         <Box>
-          <List>
-            {/* //className={classes.links} */}
+          <List sx={{ width: "100%", display: "flex" }}>
             {
               haveWebsite ? (
-                <ListItem>
-                  {/* className={classes.listItem} */}
+                <ListItem sx={{ justifyContent: "center" }}>
                   <Link href={`${website}`}>Website</Link>
                 </ListItem>
               ) : null //<span>Not yet available</span>
             }
             {
               haveRepo ? (
-                <ListItem>
-                  {/* className={classes.listItem} */}
+                <ListItem sx={{ justifyContent: "center" }}>
                   <Link href={`${repo}`}>Github Repo</Link>
                 </ListItem>
               ) : null //<span>Not yet available</span>
@@ -237,6 +260,7 @@ function ProjectDetails() {
           </List>
           <Box display="flex" justifyContent="center">
             <Button
+              sx={{ flexGrow: 0 }}
               variant="contained"
               color="primary"
               onClick={() => navigate("/")}
